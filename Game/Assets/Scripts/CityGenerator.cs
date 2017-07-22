5824f8c1bd3801a3d63bfb51abfbf6cdb589eb55
Torben Dohrn <torben.dohrn@nexusger.de>
2017-07-23 00:54:31 +0200

    Add comments

--- Game/Assets/Scripts/CityGenerator.cs
+++ Game/Assets/Scripts/CityGenerator.cs
@@ -5,21 +5,49 @@ using System.Linq;
 using System.Text;
 
 namespace Game
+
 {
+
+    /// <summary>
+    /// Encapsulates logic to create a map based on text
+    /// </summary>
     public class CityGenerator
     {
+        /// <summary>
+        /// The text on which the map will be based
+        /// </summary>
+        /// <returns>The unaltered string</returns>
         public string Seed { get; private set; }
+        /// <summary>
+        /// The width of the map
+        /// </summary>
+        /// <returns>The width of the map</returns>
         public int Width { get; private set; }
+        /// <summary>
+        /// The Height of the map
+        /// </summary>
+        /// <returns>The height of the map</returns>
         public int Height { get; private set; }
 
-
+        /// <summary>
+        /// Public constructor
+        /// </summary>
+        /// <param name="seed">The text on which the map will be based</param>
+        /// <param name="width">The width of the map</param>
+        /// <param name="height">The height of the map</param>
         public CityGenerator(string seed, int width, int height)
         {
             Seed = seed;
             Width = width;
             Height = height;
         }
-
+        /// <summary>
+        /// Executes the calculation and returns the map.
+        /// </summary>
+        /// <param name="streetValue">The value which should be used for the street</param>
+        /// <param name="houseValueLowerBound">The lowest value which should be used for houses</param>
+        /// <param name="houseValueUpperBound">The highes value which should be used for houses</param>
+        /// <returns>A map with streets and random houses</returns>
         public int[] GenerateMap(int streetValue, int houseValueLowerBound, int houseValueUpperBound)
         {
 
@@ -34,6 +62,13 @@ namespace Game
             }
             return result;
         }
+        /// <summary>
+        /// Executes the calculation and return the map.
+        /// </summary>
+        /// <param name="streetValue">The value which should be used for the street</param>
+        /// <param name="houseValueLowerBound">The lowest value which should be used for houses</param>
+        /// <param name="houseValueUpperBound">The highes value which should be used for houses</param>
+        /// <returns></returns>
         private int[,] GenerateMapFromSeed(int streetValue, int houseValueLowerBound, int houseValueUpperBound)
         {
 
@@ -45,7 +80,14 @@ namespace Game
 
             return map;
         }
-
+        /// <summary>
+        /// Based on the seed, this function creates a connected path on the map
+        /// </summary>
+        /// <param name="map">The map to write the path onto</param>
+        /// <param name="startX">The start point, x</param>
+        /// <param name="startY">The start point, y</param>
+        /// <param name="streetValue">The value of the street</param>
+        /// <returns>The map with added path</returns>
         private int[,] CreatePath(int[,] map, int startX, int startY, int streetValue)
         {
 
@@ -62,7 +104,14 @@ namespace Game
 
             return map;
         }
-
+        /// <summary>
+        /// Draws the path
+        /// </summary>
+        /// <param name="map">The map to write the path onto</param>
+        /// <param name="start">The start point</param>
+        /// <param name="word">The word on which the next part of the line is based on</param>
+        /// <param name="streetColor">The street value</param>
+        /// <returns>The last position of the cursor</returns>
         private Tuple DrawThePath(int[,] map, Tuple start, string word, int streetColor)
         {
             //var color = ChooseColor(word);
@@ -76,7 +125,15 @@ namespace Game
 
             return endCursor;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="map">The map to write the path onto</param>
+        /// <param name="start">The start point</param>
+        /// <param name="color">The street value</param>
+        /// <param name="direction">The direction in which the next part of the street is to draw</param>
+        /// <param name="length">How long the next part should be drawn</param>
+        /// <returns>The last position of the cursor</returns>
         private Tuple DrawLine(int[,] map, Tuple start, int color, byte direction, int length)
         {
             int x = start.Item1;
@@ -135,17 +192,28 @@ namespace Game
             }
             return new Tuple(x, y);
         }
-
-        private Tuple SetColorOnMap(int[,] map,  int x, int y, int color)
+        /// <summary>
+        /// Set the color on one tile the map, with boundary check (Overflowing)
+        /// </summary>
+        /// <param name="map">The map to draw onto</param>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y  coordinate</param>
+        /// <param name="color">The streetvalue</param>
+        /// <returns>The cursor position (could be different from input because of overflow)</returns>
+        private Tuple SetColorOnMap(int[,] map, int x, int y, int color)
         {
             x = BorderControllWidth(x);
 
             y = BorderControllHeight(y);
 
             map[x, y] = color;
-            return new Tuple(x,y);
+            return new Tuple(x, y);
         }
-
+        /// <summary>
+        /// Helper function for overflowing
+        /// </summary>
+        /// <param name="y">Y coordinate</param>
+        /// <returns>new Y coordinate</returns>
         private int BorderControllHeight(int y)
         {
             if (y < 0)
@@ -162,7 +230,11 @@ namespace Game
             }
             return y;
         }
-
+        /// <summary>
+        /// Helper function for overflowing
+        /// </summary>
+        /// <param name="x">X coordinate</param>
+        /// <returns>new X coordinate</returns>
         private int BorderControllWidth(int x)
         {
 
@@ -181,12 +253,29 @@ namespace Game
             }
             return x;
         }
-
+        /// <summary>
+        /// Helper funktion to choose the length of the next street part based on the string length
+        /// </summary>
+        /// <param name="word">The word to check</param>
+        /// <returns>The length of the next street part</returns>
         private int ChooseLength(string word)
         {
             return word.Length;
         }
 
+/// <summary>
+/// The direction of the next street part.
+/// Based on the 8 Direction model:
+/// 1 | 0 | 7
+/// 2 | X | 6
+/// 3 | 4 | 5
+/// but adapted because we only have four directions:
+/// 0 | 0 | 3
+/// 1 | X | 3
+/// 1 | 2 | 2
+/// </summary>
+/// <param name="word">The word the direction is based on</param>
+/// <returns>The direction. between 0 and 3</returns>
         public byte ChooseDirection(string word)
         {
             var wordLength = word.Length;
@@ -196,11 +285,13 @@ namespace Game
             return (byte)(result);
         }
 
-        private byte ChooseColor(string word)
-        {
-            return 1;
-        }
-
+        /// <summary>
+        /// Initializes the map with random values
+        /// </summary>
+        /// <param name="map">The map to draw onto</param>
+        /// <param name="houseValueLowerBound">The lower bound for house values</param>
+        /// <param name="houseValueUpperBound">The higher bound for house values</param>
+        /// <returns>The map, initialized</returns>
         private int[,] FillMapWithBackGround(int[,] map, int houseValueLowerBound, int houseValueUpperBound)
         {
             var r = new Random();
