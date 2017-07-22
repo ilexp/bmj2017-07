7b135191b12cc632d31a13fd6b72336a1268b18f
Torben Dohrn <torben.dohrn@nexusger.de>
2017-07-22 19:21:17 +0200

    Press Space to strike lightning

---
+++ Game/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class WeatherController : MonoBehaviour
+    {
+
+        [SerializeField] public Lightning Zeus;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+
+                Zeus.Flash();
+            }
+        }
+    }
+}
