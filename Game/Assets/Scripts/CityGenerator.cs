b7fe2fd563dbd4dae5908c19e6c95da1e2a1f95e
Adam <adam@adamslair.net>
2017-07-22 14:29:21 +0200

    CityGenerator Mockup

---
+++ Game/Assets/Scripts/CityGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = System.Random;
+
+namespace Game
+{
+    public class CityGenerator : MonoBehaviour
+    {
+        private void CreateCube(Random rnd)
+        {
+            GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            obj.transform.position = new Vector3(rnd.Next(-5, 5), rnd.Next(-5, 5), rnd.Next(-5, 5));
+        }
+
+        private void Start()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateCube(rnd);
+            }
+        }
+    }
+}
