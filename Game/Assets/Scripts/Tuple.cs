952c1fc9ea2debefa9a1bd1b8d70318c455eb547
Torben Dohrn <torben.dohrn@nexusger.de>
2017-07-22 18:13:52 +0200

    Add text basedgenerator

---
+++ Game/Assets/Scripts/Tuple.cs
@@ -0,0 +1,12 @@
+namespace Game{
+    public struct Tuple{
+        public int Item1;
+        public int Item2; 
+
+        public Tuple(int item1, int item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+}
